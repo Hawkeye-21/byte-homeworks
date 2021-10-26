2c45674e9493d4e27d35a75ebc3e767996711b86
Hawkeye-21 <yulya@email.com>
2021-10-27 00:55:02 +0300

    hw2

---
+++ homeworl_2/script.js
@@ -0,0 +1,22 @@
+// First
+
+let userAge = 25;
+const userName = "Patrik";
+const userSurname = "Schick";
+
+//Second
+
+let newUser;
+let purchaseList;
+let userOnline;
+let employeeSalary;
+let mouseCursorCoordinates;
+
+//Third
+
+let message = "Hello World!";
+console.log(message);
+
+console.log('userAge:', userAge);
+console.log('userName:', userName);
+console.log('userSurname:', userSurname);
